cf1cac1e61a3aa3f8f1662bc33e53be5a72cbef1
Arijit Layek <notmyemailid@example.com>
2015-03-31 18:57:22 +0000

    Add dummy data to the table

--- server.js
+++ server.js
@@ -6,8 +6,34 @@ var app = express();
 // middleware
 app.use(express.static(path.join(__dirname + '/views')));
 
-app.get('/*', function(req, res){
+// create dummy data
+  var contactList = [
+    {
+      'name' : 'Tim',
+      'email': 'tim@example.com',
+      'contact' : '(111)-111 1111'
+    },
+    {
+      'name' : 'Michael',
+      'email': 'michael@example.com',
+      'contact' : '(223)-134 3542'
+    },
+    {
+      'name' : 'John',
+      'email': 'john@example.com',
+      'contact' : '(532)-986 9923'
+    },
+    {
+      'name' : 'Jessie',
+      'email': 'jes@example.com',
+      'contact' : '(176)-111 8651'
+    }
+  ];
+  
+
+app.get('/contactList', function(req, res){
   console.log("request received");
+  res.json(contactList);
 });
 
 var port = process.argv[2] || process.env.PORT || 3000;
