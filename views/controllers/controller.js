741504c2151a2be774f110bf0ae96dab9b48350a
Arijit Layek <notmyemailid@example.com>
2015-03-31 20:20:51 +0000

    Remove all console.log, except one

--- views/controllers/controller.js
+++ views/controllers/controller.js
@@ -1,10 +1,8 @@
 var myApp = angular.module('myApp', []);
 myApp.controller('AppControl', ['$scope', '$http', function($scope, $http){
-  console.log("Hello World from Controller");
   // route for the data
   // essentially, raise a request for the path
   $http.get('/contactList').success(function(response){
-    console.log("Received data successfully");
     $scope.contactList = response;
   });
   
