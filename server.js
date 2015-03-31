f97eec31c61028ef116697efe065ca03ca405fd9
Arijit Layek <notmyemailid@example.com>
2015-03-31 20:19:18 +0000

    Use DB URL and send partial data, with the _id field

--- server.js
+++ server.js
@@ -1,39 +1,23 @@
 var express = require('express');
 var path  = require('path');
+// db connection
+var mongojs = require('mongojs');
+var db = mongojs(process.env.MONGODB_DEVELOPMENT_USERNAME + ':' + process.env.MONGODB_DEVELOPMENT_PASSWORD + '@' + process.env.MONGODB_DEVELOPMENT_HOST + ':' + process.env.MONGODB_DEVELOPMENT_PORT + '/' + process.env.MONGODB_DEVELOPMENT_DB, ['contactList']);
 
 var app = express();
 
 // middleware
 app.use(express.static(path.join(__dirname + '/views')));
 
-// create dummy data
-  var contactList = [
-    {
-      'name' : 'Tim',
-      'email': 'tim@example.com',
-      'contact' : '(111)-111 1111'
-    },
-    {
-      'name' : 'Michael',
-      'email': 'michael@example.com',
-      'contact' : '(223)-134 3542'
-    },
-    {
-      'name' : 'John',
-      'email': 'john@example.com',
-      'contact' : '(532)-986 9923'
-    },
-    {
-      'name' : 'Jessie',
-      'email': 'jes@example.com',
-      'contact' : '(176)-111 8651'
-    }
-  ];
-  
-
 app.get('/contactList', function(req, res){
-  console.log("request received");
-  res.json(contactList);
+  db.contactList.find(function(err, data){
+    if(err){
+      console.log("Something went wrong while getting data from DB");
+    }
+    res.json(data.map(function(item){
+      return {name:item.name, email:item.email, contact:item.contact};
+    }));
+  });
 });
 
 var port = process.argv[2] || process.env.PORT || 3000;
